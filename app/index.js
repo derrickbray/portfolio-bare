fc172e7964bdedef4a6073e2765c79e2a148400e
Derrick Bray <derrickbray@yahoo.com>
2017-05-10 18:15:06 -0500

    play with js for console

--- app/index.js
+++ app/index.js
@@ -1,9 +1,13 @@
 import 'whatwg-fetch';
 
-const hello = 'hello world';
+const hello = 'Hey, what are you doing here?';
+const contact = 'If you like my site let me know derrickbray82@gmail.com';
+const not = 'If you dont like my site you dont have to say anything.';
 
 function message() {
   console.log(hello);
+  console.log(contact);
+  console.log(not);
 }
 
 message();
